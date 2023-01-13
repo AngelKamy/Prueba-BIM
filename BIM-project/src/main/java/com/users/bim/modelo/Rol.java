3db314c1736ede6736af52cfdc4561dfecb16e16
AngelKamy <ivanvaq3@gmail.com>
2023-01-12 18:22:09 -0600

    feat: creación de entidades DB

--- BIM-project/src/main/java/com/users/bim/modelo/Rol.java
+++ BIM-project/src/main/java/com/users/bim/modelo/Rol.java
@@ -1,5 +1,43 @@
 package com.users.bim.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rol")
 public class Rol {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public Rol(Long id, String usuario) {
+		super();
+		this.id = id;
+		this.usuario = usuario;
+	}
+
+	public Rol() {
+		super();
+	}
 
 }
--- BIM-project/src/main/java/com/users/bim/modelo/usuario.java
+++ BIM-project/src/main/java/com/users/bim/modelo/usuario.java
@@ -2,33 +2,102 @@ package com.users.bim.modelo;
 
 import java.util.Collection;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "usuarios", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class usuario {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@Column(name="nombre")
-	private String nombre;
-	
-	@Column(name= "email")
+
+	@Column(name = "email")
 	private String email;
-	
-	@Column(name= "password")
+
+	@Column(name = "usuario")
+	private String usuario;
+
+	@Column(name = "password")
 	private String password;
 	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(
+			name = "usuarios_roles",
+			joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+			)
 	
 	private Collection<Rol> roles;
-	
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Collection<Rol> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Collection<Rol> roles) {
+		this.roles = roles;
+	}
+
+	public usuario(Long id, String email, String usuario, String password, Collection<Rol> roles) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.usuario = usuario;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public usuario(String email, String usuario, String password, Collection<Rol> roles) {
+		super();
+		this.email = email;
+		this.usuario = usuario;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public usuario() {
+		super();
+	}
+
 }
