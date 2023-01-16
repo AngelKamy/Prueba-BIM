e662811e93739f1d615c819f4e5a3601475ac2f8
AngelKamy <ivanvaq3@gmail.com>
2023-01-15 21:03:38 -0600

    feat: Actualización DB

--- BIM-project/src/main/java/com/users/bim/modelo/usuario.java
+++ BIM-project/src/main/java/com/users/bim/modelo/usuario.java
@@ -9,6 +9,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
@@ -34,7 +35,8 @@ public class usuario {
 	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	@JoinTable(
 			name = "usuarios_roles",
-			joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id")
+			joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id"),
+			inverseJoinColumns = @JoinColumn(name = "rol_id", referencedColumnName = "id")
 			)
 	
 	private Collection<Rol> roles;
